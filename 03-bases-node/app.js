3372103d4548b02538efdd21c8b2b45e0da2c824
DiegoDiem <ldmg.9@hotmail.com>
2023-01-17 22:34:37 -0600

    Tabla del 3 agregado

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,21 @@
+const fs= require('fs');
+
 console.clear();
+const base = 3;
 console.log("===============================")
-console.log('Tabla del 5')
+console.log(`Tabla del ${base}`)
 console.log("===============================")
 
-const base = 5;
+
+let salida= '';
 for (let i = 0; i < 11; i++) {
-    console.log(`${base} x ${i} = ${base*i}`) 
+    salida+= `${base} x ${i} = ${base*i} \n`;
     
 }
+
+console.log(salida)
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err)=>{
+    if (err) throw err;
+    console.log(`tabla-${base}.txt creadoo!!`)
+});
